7f08066dabdea58630a8515539779555488e4f00
Bryan A Ford <brynosaurus@gmail.com>
2019-10-12 12:09:18 +0200

    CBS: add encode function, and test suite

---
+++ cbs.go
@@ -0,0 +1,112 @@
+package cbs
+
+import (
+	"errors"
+	"io"
+)
+
+// Blob-encode a byte slice src and append it to slice dst.
+// Allocates and returns a new destination buffer
+// if the blob-encoded data does not fit into dst.
+//
+// This function supports encoding only small slices
+// whose content is less than 16KiB in size.
+// To encode large slices, use the streaming-capable Encoder.
+//
+func Encode(dst, src []byte) []byte {
+
+	// 1-byte header encoding
+	n := len(src)
+	if n == 1 && src[0] < 128 {
+		return append(dst, src...)	// value is the blob encoding
+	}
+	if n < 128 {				// shorter than 2^7 bytes
+		dst := append(dst, byte(128+n))
+		return append(dst, src...)
+	}
+
+	// 3-byte header encoding
+	if n < 16384 {				// shorter than 2^7+2^16 bytes
+		n -= 128
+		dst := append(dst, 0x81, byte(n >> 8), byte(n))
+		return append(dst, src...)
+	}
+
+	panic("content too large for small-blob encoder")
+}
+
+// Decode the header of a small blob, whose content is of size less than 16KiB,
+// from the start of a byte slice.
+// On success, returns the offset in the byte slice
+// and the length in bytes of the blob's content.
+//
+// This function returns EOF if the provided byte string
+// does not contain a complete blob header.
+//
+// This decoding function does not support large blobs of 16KiB or more,
+// which may require the decoder to handle multi-part streaming encodings.
+// On see a blob header for a large blob, this function returns an error.
+// To decode large blobs of 16KiB or more, use the streaming-capable Decoder.
+// 
+func Header(buf []byte) (contentOfs, contentLen int, err error) {
+
+	// 1-byte headers
+	if len(buf) == 0 {
+		return 0, 0, EOF
+	}
+	if buf[0] < 128 {		// 0vvvvvvv direct value encoding
+		return 0, 1, nil	// the header is the 1-byte content
+	}
+	if buf[0] != 129 {		// 1nnnnnnn encoding for n != 1
+		return 1, int(buf[0]-128), nil
+	}
+
+	// 2-byte headers
+	if len(buf) < 2 {
+		return 0, 0, EOF
+	}
+	if buf[1] >= 128 {		// 10000001,1vvvvvvv encoding
+		return 1, 1, nil
+	}
+
+	// 3-byte headers
+	if len(buf) < 3 {
+		return 0, 0, EOF
+	}
+	if buf[1] < 64 {		// 10000001,00nnnnnn,nnnnnnnn
+		return 3, 128 + (int(buf[1]) << 8) + int(buf[2]), nil
+	}
+
+	// 4-byte streaming-capable headers
+	return 0, 0, TooLong
+}
+
+// Decode the header of a small blob, whose content is of size less than 16KiB,
+// from the start of a byte slice.
+// On success, returns a sub-slice of the blob containing the content,
+// and a disjoint sub-slice containing the remainder of the buffer
+// immediately following the decoded blob.
+//
+// This function returns EOF if the provided byte string
+// does not contain a complete blob.
+//
+// This decoding function does not support large blobs of 16KiB or more,
+// which may require the decoder to handle multi-part streaming encodings.
+// On see a blob header for a large blob, this function returns an error.
+// To decode large blobs of 16KiB or more, use the streaming-capable Decoder.
+// 
+func Decode(buf []byte) (content, remainder []byte, err error) {
+	ofs, n, err := Header(buf)
+	if err != nil {
+		return nil, nil, err
+	}
+	if len(buf) < ofs+n {
+		return nil, nil, EOF
+	}
+	return buf[ofs:ofs+n], buf[ofs+n:], nil
+}
+
+var EOF = io.EOF
+
+var TooLong = errors.New("blob content too long")
+
---
+++ cbs_test.go
@@ -0,0 +1,158 @@
+package cbs
+
+import (
+	"testing"
+	"bytes"
+	"math/rand"
+)
+
+type testCase struct {
+	data, blob []byte
+}
+
+var testCases = []testCase{
+	{ []byte{},		[]byte{0x80} },		// empty blob
+
+	{ []byte{0},		[]byte{0x00} },		// 1-byte content
+	{ []byte{1},		[]byte{0x01} },
+	{ []byte{0x7e},		[]byte{0x7e} },
+	{ []byte{0x7f},		[]byte{0x7f} },
+	{ []byte{0x80},		[]byte{0x81,0x80} },
+	{ []byte{0x81},		[]byte{0x81,0x81} },
+	{ []byte{0xfe},		[]byte{0x81,0xfe} },
+	{ []byte{0xff},		[]byte{0x81,0xff} },
+
+	{ []byte{0x00,0x00},	[]byte{0x82,0x00,0x00} },	// 2-byte
+	{ []byte{0xab,0xcd},	[]byte{0x82,0xab,0xcd} },
+	{ []byte{0xff,0xff},	[]byte{0x82,0xff,0xff} },
+
+	// 3-byte
+	{ []byte{0xab,0xcd,0xef}, []byte{0x83,0xab,0xcd,0xef} },
+
+	// 4-byte
+	{ []byte{0xde,0xad,0xbe,0xef}, []byte{0x84,0xde,0xad,0xbe,0xef} }, 
+
+	// 8-byte
+	{ []byte{0xde,0xad,0xbe,0xef,0x4b,0xad,0xf0,0x0d},
+	  []byte{0x88,0xde,0xad,0xbe,0xef,0x4b,0xad,0xf0,0x0d} }, 
+
+	// 123-byte
+	{ []byte("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+	  []byte("\xfbLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.") },
+
+	// 127-byte
+	{ []byte("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dol"),
+	  []byte("\xffLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dol") },
+
+	// 128-byte
+	{ []byte("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolo"),
+	  []byte("\x81\x00\x00Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolo") },
+
+	// 204-byte
+	{ []byte("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat."),
+	  []byte("\x81\x00\x4cLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat.") },
+
+	// 778-byte
+	{ []byte("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat. Nunc aliquet bibendum enim facilisis gravida. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Amet luctus venenatis lectus magna fringilla. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Sagittis orci a scelerisque purus semper eget duis. Nulla pharetra diam sit amet nisl suscipit. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. Fusce ut placerat orci nulla. Pharetra vel turpis nunc eget lorem dolor. Tristique senectus et netus et malesuada."),
+	  []byte("\x81\x02\x8aLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat. Nunc aliquet bibendum enim facilisis gravida. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Amet luctus venenatis lectus magna fringilla. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Sagittis orci a scelerisque purus semper eget duis. Nulla pharetra diam sit amet nisl suscipit. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. Fusce ut placerat orci nulla. Pharetra vel turpis nunc eget lorem dolor. Tristique senectus et netus et malesuada.") },
+
+	// 4KiB-byte
+	bigTestCase(4096, 0, []byte{0x81,0x0f,0x80}),
+	bigTestCase(4096, 1, []byte{0x81,0x0f,0x80}),
+	bigTestCase(4096, 0xff, []byte{0x81,0x0f,0x80}),
+	bigTestCase(4096, -1, []byte{0x81,0x0f,0x80}),	// random data
+
+	// 16383-byte, largest canonical medium-size blob
+	bigTestCase(16383, -1, []byte{0x81,0x3f,0x7f}),
+
+	// 16384-byte, smallest non-canonical streamable blob
+	bigTestCase(16384, -1, []byte{0x81,0x3f,0x80}),
+}
+
+var largeCases = []testCase{
+
+	// 16511-byte, largest blob representable with 3-byte header
+	bigTestCase(16511, -1, []byte{0x81,0x3f,0xff}),
+
+	// 16512-byte, smallest blob requiring 4-byte header
+	bigTestCase(16512, -1, []byte{0x81,0x40,0x40,0x00}),
+}
+
+
+func bigTestCase(n, v int, hdr []byte) testCase {
+	h := len(hdr)
+	buf := make([]byte, h + n)
+	copy(buf, hdr)
+	if v > 0 {
+		for i := range buf[h:] {
+			buf[h+i] = byte(v)
+		}
+	} else if v < 0 {
+		rand.Read(buf[h:])
+	}
+	return testCase{ data: buf[h:], blob: buf }
+}
+
+
+func TestEncode(t *testing.T) {
+	var accx, accy []byte
+	for i, st := range testCases {
+
+		// Skip tests on large blobs requiring the streaming encoder
+		if len(st.data) >= 16384 {
+			continue
+		}
+
+		// Test encoding into a fresh buffer
+		blob := Encode(nil, st.data)
+		if bytes.Compare(blob, st.blob) != 0 {
+			t.Errorf("incorrect encode in small test %v", i)
+		}
+
+		// Test encoding cumulatively
+		accx = Encode(accx, st.data)
+		accy = append(accy, st.blob...)
+		if bytes.Compare(accx, accy) != 0 {
+			t.Errorf("incorrect encode in small test %v", i)
+		}
+	}
+}
+
+func TestDecode(t *testing.T) {
+
+	// First just decode each reference blob individually
+	for i, st := range testCases {
+		data, rem, err := Decode(st.blob)
+		if err != nil {
+			t.Error(err)
+		}
+		if bytes.Compare(data, st.data) != 0 {
+			t.Errorf("incorrect decode in small test %v", i)
+		}
+		if len(rem) != 0 {
+			t.Errorf("failed to consume everything in test %v", i)
+		}
+	}
+
+	// concatenate all the small-test reference blobs
+	var buf []byte
+	for _, st := range testCases {
+		buf = append(buf, st.blob...)
+	}
+
+	// now progressively decode and check each
+	for i, st := range testCases {
+		data, rem, err := Decode(buf)
+		if err != nil {
+			t.Error(err)
+		}
+		if bytes.Compare(data, st.data) != 0 {
+			t.Errorf("incorrect decode in small test %v", i)
+		}
+		buf = rem
+	}
+	if len(buf) != 0 {
+		t.Error("failed to decode everything in concatenated decode")
+	}
+}
+
